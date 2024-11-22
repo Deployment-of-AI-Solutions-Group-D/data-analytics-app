66d2277c6267961a0bb9e4eea15fe63c3fbdb7b1
Mathew V Manoj <mathew.manoj1995@gmail.com>
2024-11-22 16:34:18 -0500

    Adding templates

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 import analysis
+import os
 
 app = Flask(__name__)
+print("Template folder path:", os.path.join(os.getcwd(), 'templates'))
 
 @app.route('/')
 def index():
+    print("Current Working Directory: ", os.getcwd())
     results = analysis.run_analysis()
     return render_template('index.html', results=results)
 
